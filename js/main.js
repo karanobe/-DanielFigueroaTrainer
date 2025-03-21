e1bfc3f91a3563f12d2e3a7d5bd4185eb860c4e6
Karan Oberoi <koberoi1017@gmail.com>
2025-03-20 23:08:32 -0700

    client inquiry form

--- js/main.js
+++ js/main.js
@@ -171,4 +171,106 @@
         });
     });
 
+    $( "#contact" ).on( "submit", function( event ) {
+        event.preventDefault();
+        $('#submit').hide()
+        $('.submitting').show()
+
+
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const email = document.getElementById('email').value;
+        const age = document.getElementById('age').value;
+        const goals = document.getElementById('goals').value;
+
+        const botpoison = new Botpoison({
+        publicKey: "pk_5635f6ea-7c6f-42dc-ad11-b79d1f4a75b1",
+        });
+        botpoison.challenge().then(({ solution }) => {
+        axios
+          .post("https://submit-form.com/FvGRJSBE2", {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            age: age,
+            goals: goals,
+            _botpoison: solution,
+          })
+          .then(function (response) {
+            $('#contact').fadeOut(400, function () {
+                $('.success .firstName').html(firstName)
+                $('.success').fadeIn();
+            });
+          })
+          .catch(function (response) {
+            console.error(response);
+            $('#contact').fadeOut(400, function () {
+                $('.failure .firstName').html(firstName)
+                $('.failure').fadeIn();
+            });            
+          });
+        });      
+    });
+
+    $( "#lead" ).on( "submit", function( event ) {
+        event.preventDefault();
+        $('#submit').hide()
+        $('.submitting').show()
+
+
+        const email = document.getElementById('email').value;
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const height = document.getElementById('height').value;
+        const age = document.getElementById('age').value;
+        const weight = document.getElementById('weight').value;
+        const goals = document.getElementById('goals').value;
+        const hurdles = document.getElementById('hurdles').value;
+        const weightFluctuation = document.getElementById('weightFluctuation').value;
+        const homeLife = document.getElementById('homeLife').value;
+        const serious = document.getElementById('serious').value;
+        const importance = document.getElementById('importance').value;
+        const occupation = document.getElementById('occupation').value;
+        const referral = document.getElementById('referral').value;
+        const phone = document.getElementById('phone').value;
+
+        const botpoison = new Botpoison({
+        publicKey: "pk_5635f6ea-7c6f-42dc-ad11-b79d1f4a75b1",
+        });
+        botpoison.challenge().then(({ solution }) => {
+        axios
+          .post("https://submit-form.com/FvGRJSBE2", {
+            email: email,
+            phone: phone,
+            first_name: firstName,
+            last_name: lastName,
+            age: age,
+            height: height,
+            weight: weight,
+            goals: goals,
+            hurdles: hurdles,
+            weight_fluctuation: weightFluctuation,
+            home_life: homeLife,
+            how_serious: serious,
+            importance: importance,
+            occupation: occupation,
+            referral: referral,
+            _botpoison: solution,
+          })
+          .then(function (response) {
+            $('#lead').fadeOut(400, function () {
+                $('.success .firstName').html(firstName)
+                $('.success').fadeIn();
+            });
+          })
+          .catch(function (response) {
+            console.error(response);
+            $('#lead').fadeOut(400, function () {
+                $('.failure .firstName').html(firstName)
+                $('.failure').fadeIn();
+            });            
+          });
+        });      
+    });
+
 })(jQuery);
